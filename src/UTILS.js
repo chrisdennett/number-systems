1d1be06db871a505edc0b45a5493e83e22178f09
Chris Dennett <chrisdennett@gmail.com>
2019-04-04 14:43:36 +0100

    Updated utils to work with any base number system.

--- src/UTILS.js
+++ src/UTILS.js
@@ -29,3 +29,52 @@ export const createBinaryColumns = (maxValue = maxValue) => {
 
   return binaryCols;
 };
+
+export const createNumberSystemColumns = (base = 10, max = 9999) => {
+  const cols = [];
+  let currentValue = 1;
+
+  while (currentValue < max) {
+    cols.unshift(currentValue);
+
+    currentValue *= base;
+  }
+
+  return cols;
+};
+
+export const getNumberSystemArr = (num = 99, base = 10) => {
+  // e.g. num = 8999
+  let colValue = 1;
+  const cols = [{ col: colValue }];
+
+  // create the array of columns by adding a column for 1's
+  // then multiply curr column number by the base to get the next col
+  // repeat until the number of the column to be made is bigger than the number
+  while (colValue * base <= num) {
+    colValue *= base;
+    cols.unshift({ col: colValue });
+  }
+
+  // then go through those columns in reverse and minus the value of the column
+  // from the number as base times or until the column number can't be removed any
+  // more.
+  for (let colObj of cols) {
+    let colCount = 0;
+
+    for (let j = 0; j < base; j++) {
+      if (num >= colObj.col) {
+        num -= colObj.col;
+        colCount++;
+      }
+    }
+
+    colObj.count = colCount;
+  }
+
+  return cols;
+};
+
+const arr = getNumberSystemArr();
+console.log(arr.map(c => c.col));
+console.log(arr.map(c => c.count));
